c2b72dfc18fdb05ae5238d538e5cfa8eeec9b8a6
Yui-M-K <your-email@example.com>
2025-10-23 18:00:59 +0900

    とんかつはくるめしのみ対応

--- 03_メール内容印刷.js
+++ 03_メール内容印刷.js
@@ -1,18 +1,10 @@
-// 現時点はワオ、くるめし、シェフコレだけ対応
-
 function runPrintMails() {
   try {
     Logger.log("-----メール印刷処理を開始します-----");
-    
-    var CONFIG = [
-      CONFIG_NARIKURA_WAO,
-      CONFIG_NARIKURA_KURUMESHI,
-      CONFIG_NARIKURA_CHEF_COLLE,
-    ];
-    
+    var CONFIG = [ CONFIG_NARIKURA_KURUMESHI];
     CommonLib.printMails(CONFIG);
-    
     Logger.log("-----メール印刷処理が完了しました-----");
+
   } catch (e) {
     Logger.log("⚠️メール印刷処理でエラーが発生しました: " + e.message);
     Logger.log("エラー詳細: " + e.stack);
