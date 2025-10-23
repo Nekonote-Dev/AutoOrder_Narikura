429a79796417650d9813482bf1247594cf461b10
Yui-M-K <your-email@example.com>
2025-10-23 18:03:41 +0900

    format fix

--- 03_メール内容印刷.js
+++ 03_メール内容印刷.js
@@ -1,10 +1,10 @@
 function runPrintMails() {
   try {
     Logger.log("-----メール印刷処理を開始します-----");
-    var CONFIG = [ CONFIG_NARIKURA_KURUMESHI];
+    var CONFIG = [CONFIG_NARIKURA_KURUMESHI];
     CommonLib.printMails(CONFIG);
     Logger.log("-----メール印刷処理が完了しました-----");
-
+    
   } catch (e) {
     Logger.log("⚠️メール印刷処理でエラーが発生しました: " + e.message);
     Logger.log("エラー詳細: " + e.stack);
